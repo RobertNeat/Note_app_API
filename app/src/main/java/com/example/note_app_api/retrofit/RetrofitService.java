1f90aea16bb440a53f2b54458f39ba7b96dfcf3b
Robert Sereda <robertgoogl@gmail.com>
2024-03-10 21:40:31 +0100

    added retrofit config

---
+++ app/src/main/java/com/example/note_app_api/retrofit/RetrofitService.java
@@ -0,0 +1,25 @@
+package com.example.note_app_api.retrofit;
+
+import com.google.gson.Gson;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+//configuration for retrofit (making CRUD requests to REST API)
+public class RetrofitService {
+    private Retrofit retrofit;
+    public RetrofitService(){
+        initializeRetrofit();
+    }
+
+    private void initializeRetrofit() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.0.159:9000")//set the IP of the computer that runs the REST API server
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .build();
+    }
+
+    public Retrofit getRetrofit() {
+        return retrofit;
+    }
+}
